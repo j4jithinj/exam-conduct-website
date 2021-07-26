1904a067ede0357e682ba471912b4c314b6269e7
JITHIN J <j4jithinj@gmail.com>
2021-07-26 22:17:50 +0530

    new

--- examsite/settings.py
+++ examsite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'examsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'examdb',
+        'USER':'postgres',
+        'PASSWORD':'quest123',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
