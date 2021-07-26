af5e2754da352b32865360dc4d1d0c88b506b001
JITHIN J <j4jithinj@gmail.com>
2021-07-26 23:50:03 +0530

    login completred

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
+from os import name
 from django.urls import path, include
 from . import views as v
 
 urlpatterns = [
     path('', v.userlogin, name='userlogin'),
     path('signup/', v.usersignup, name='usersignup'),
+    path('profile/', v.profile, name='profile'),
+    path('logout/', v.userlogout, name='userlogout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,39 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def userlogin(request):
+    if request.method == 'POST':
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile')
     return render(request, 'accounts/login.html', {})
 
 def usersignup(request):
     form = UserCreationForm(request.POST or None)
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        firstname = request.POST.get('fname')
+        lastname = request.POST.get('lname')
+        password = request.POST.get('password')
+        user = User.objects.create_user(email, email, password)
+        user.firs_tname = firstname
+        user.last_name = lastname
+        user.save()
+        return redirect('userlogin')
     return render(request, 'accounts/signup.html', {'form':form})
+
+def userlogout(request):
+    logout(request)
+    return redirect('userlogin')
+
+@login_required(login_url='userlogin')
+def profile(request):
+    return render(request, 'accounts/dashboard.html')
