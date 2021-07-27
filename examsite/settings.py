f8884fa354b3dfecb0053bcadcc3ee2351f473ae
JITHIN <j4jithinj@gmail.com>
2021-07-27 23:35:01 +0530

    newone

--- examsite/settings.py
+++ examsite/settings.py
@@ -140,7 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
