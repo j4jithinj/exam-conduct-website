dea4786741b926dd5342729fa16dd17e3205e9d0
JITHIN <j4jithinj@gmail.com>
2021-07-27 23:08:17 +0530

    for heroku new

--- examsite/settings.py
+++ examsite/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'accounts',
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
